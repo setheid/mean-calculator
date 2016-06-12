c2d2145a3534359b9c7ed2ac947dfe06ccc25b17
setheid <setheid@gmail.com>
2016-06-12 15:03:29 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Automatic solving
+
+Dropdown menu:
+Add, subtract, multiply, divide.
+
+Two input boxes that take numbers.
+
+Calculate button that sends method name and numbers to the back end as json.
+
+Dropdown choices come from rest api.
+
+Display calculated result.
---
+++ server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+var operators = ['Add', 'Subtract', 'Multiply', 'Divide'];
+
+function calculate(operator, value1, value2) {
+  if ( operator == 'Divide' && value2 == 0) return 'Undefined';
+
+  if ( operator == 'Add' )      return value1 + value2;
+  if ( operator == 'Subtract' ) return value1 - value2;
+  if ( operator == 'Multiply' ) return value1 * value2;
+  if ( operator == 'Divide' )   return value1 / value2;
+}
+
+app.use(express.static(__dirname + '/build'));
+app.use(require('body-parser').json());
+
+app.listen(8080, () => {
+  console.log('server started on port 8080');
+});
+
+app.route('/calculator')
+
+  .get((req, res) => {
+    res.json({
+      operators
+    });
+  })
+
+  .post((req, res) => {
+    var operator  = req.body.operator;
+    var value1    = req.body.value1;
+    var value2    = req.body.value2;
+
+    var result = calculate(operator, value1, value2);
+
+    res.json({
+      result
+    })
+  });
